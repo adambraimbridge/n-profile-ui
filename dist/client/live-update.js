1f5ef58f99319d74bbd513e73b2d040d46234e09
doramatadora <dora.militaru@ft.com>
2018-04-24 10:12:00 +0100

    update dist  🐿 v2.8.0

--- dist/client/live-update.js
+++ dist/client/live-update.js
@@ -1,20 +1,20 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const consent_1 = require("./consent");
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const consent_1 = require('./consent');
 class LiveUpdateConsent extends consent_1.ConsentForm {
-    constructor(opts) {
-        super(opts);
-        if (this.submitButton) {
-            this.submitButton.style.display = 'none';
-        }
-    }
-    onChange(callback) {
-        this.radios.forEach((radio) => {
-            radio.addEventListener('change', (e) => {
-                const consent = this.consentFromRadio(radio);
-                callback(consent, e);
-            });
-        });
-    }
+	constructor (opts) {
+		super(opts);
+		if (this.submitButton) {
+			this.submitButton.style.display = 'none';
+		}
+	}
+	onChange (callback) {
+		this.radios.forEach((radio) => {
+			radio.addEventListener('change', (e) => {
+				const consent = this.consentFromRadio(radio);
+				callback(consent, e);
+			});
+		});
+	}
 }
 exports.LiveUpdateConsent = LiveUpdateConsent;
--- dist/client/reconsent.js
+++ dist/client/reconsent.js
@@ -1,33 +1,33 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const update_on_save_1 = require("./update-on-save");
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const update_on_save_1 = require('./update-on-save');
 class Reconsent extends update_on_save_1.UpdateConsentOnSave {
-    constructor(opts) {
-        super(opts);
-        if (opts.flag === 'autoload') {
-            this.overlaySetup();
-        }
-        else if (opts.flag === 'banner') {
-            this.bannerSetup();
-        }
-    }
-    bannerSetup() {
-        const banner = document.querySelector('.consent-banner__outer');
-        const bannerButton = document.querySelector('.consent-banner__button');
-        banner.classList.add('active');
-        bannerButton.addEventListener('click', () => {
-            banner.classList.remove('active');
-            this.overlaySetup();
-        });
-    }
-    overlaySetup() {
-    }
-    overlayCloseHandler() {
-        const closeConsentForm = document.querySelector('.consent-form__close');
-        closeConsentForm.addEventListener('click', () => {
-            this.consentOverlay.close();
-        });
-    }
+	constructor (opts) {
+		super(opts);
+		if (opts.flag === 'autoload') {
+			this.overlaySetup();
+		}
+		else if (opts.flag === 'banner') {
+			this.bannerSetup();
+		}
+	}
+	bannerSetup () {
+		const banner = document.querySelector('.consent-banner__outer');
+		const bannerButton = document.querySelector('.consent-banner__button');
+		banner.classList.add('active');
+		bannerButton.addEventListener('click', () => {
+			banner.classList.remove('active');
+			this.overlaySetup();
+		});
+	}
+	overlaySetup () {
+	}
+	overlayCloseHandler () {
+		const closeConsentForm = document.querySelector('.consent-form__close');
+		closeConsentForm.addEventListener('click', () => {
+			this.consentOverlay.close();
+		});
+	}
 }
 exports.Reconsent = Reconsent;
 ;
