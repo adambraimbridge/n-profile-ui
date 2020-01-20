f117b2680687cdd01c1a5d7c95aba0daf4ff8f85
John Flockton <j.r.flockton@gmail.com>
2020-01-20 13:31:10 +0000

    Add dep 🐿 v2.12.5

---
+++ dist/commonjs/client/message.js
@@ -0,0 +1,34 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const o_message_1 = require('o-message');
+class ConsentMessage {
+	constructor (options) {
+		this.options = options;
+		const element = document.querySelector(`${this.options.selector} > .o-message`);
+		if (!element) {
+			throw new Error('Invalid selector');
+		}
+		this.element = element;
+		this._message = new o_message_1.default(this.element, {
+			messageClass: 'o-message'
+		});
+		if (this.options.hideOnInit) {
+			this.hide();
+		}
+	}
+	hide () {
+		this.element.classList.add('consent-message--hidden');
+	}
+	show () {
+		this.element.classList.remove('consent-message--hidden');
+	}
+	init (showMessage) {
+		if (showMessage) {
+			this.show();
+		}
+		else {
+			this.hide();
+		}
+	}
+}
+exports.ConsentMessage = ConsentMessage;
--- dist/esm/server/main.js
+++ dist/esm/server/main.js
@@ -1,21 +1,14 @@
 import 'isomorphic-fetch';
 import * as helpers from '../helpers';
-import { FowAPI } from '../types/fow-api';
-
-export async function getFormOfWords(
-	name: string,
-	scope: string = 'FTPINK'
-): Promise<FowAPI.Fow> {
-	if(!process.env.FOW_API_HOST) {
+export async function getFormOfWords (name, scope = 'FTPINK') {
+	if (!process.env.FOW_API_HOST) {
 		throw new Error('Missing FOW_API_HOST environment variable');
 	}
 	const url = `${process.env.FOW_API_HOST}/api/v1/${scope}/${name}`;
 	const fow = await fetch(url, {
 		timeout: 2000
-	} as RequestInit);
+	});
 	const json = await fow.json();
-	
 	return json;
 }
-
 export { helpers };
