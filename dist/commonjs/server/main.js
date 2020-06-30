ba53a40d6fac1346f94fcc5aeb30be4d6ed70492
Tye, Thurston <thurston.tye@ft.com>
2020-06-30 10:15:59 +0100

    Revert "add aria-label= to yes no toggle 🐿 v2.13.0"
    This reverts commit 5402d400b04a5af39e5214a6047905ad2d3485ae.

--- dist/commonjs/server/main.js
+++ dist/commonjs/server/main.js
@@ -1,6 +1,5 @@
 'use strict';
 Object.defineProperty(exports, '__esModule', { value: true });
-exports.helpers = exports.getFormOfWords = void 0;
 require('isomorphic-fetch');
 const helpers = require('../helpers');
 exports.helpers = helpers;
